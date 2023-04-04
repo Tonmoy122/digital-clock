7c1ec77e59b7bad4ce681581141aa56ef23b2f56
Tonmoy122 <mehrabtonmoy49@gmail.com>
2023-04-04 10:40:04 +0600

    added digital clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let time = new Date().toLocaleTimeString()
+
+  const [currentTime, setCurrentTime] = useState(time)
+
+  const updateTime = () => {
+    let time = new Date().toLocaleTimeString()
+    setCurrentTime(time)
+  }
+  setInterval(updateTime, 1000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin: "15%",
+      padding: "5%",
+      background: "#200506",
+      color: "#fff",
+      display: "grid",
+      justifyContent: "center",
+      border: "2px solid #ff5912"
+    }}>
+      <h1>Digital Clock</h1>
+      <h2>{currentTime}</h2>
+      {/* <button onClick={updateTime}>Get Time</button> */}
     </div>
   );
 }
